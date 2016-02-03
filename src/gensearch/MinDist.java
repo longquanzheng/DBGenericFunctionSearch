d5e2b71fce39065e1d7f61651062c87575cb2330
Q. Long <qlong@purdue.edu>
2016-02-03 15:06:50 -0500

    done mindist, minmaxdist

--- src/gensearch/MinDist.java
+++ src/gensearch/MinDist.java
@@ -1,10 +1,60 @@
 package gensearch;
 
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
 public class MinDist {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Expression exp = new ExpressionBuilder("x1*x2*x3").variables("x1","x2","x3").build();
+		double[] mbrS={1,2,3}, mbrT={4,5,6};
+		RTreeNode rtn = new RTreeNode();
+		rtn.MBR_S = mbrS;
+		rtn.MBR_T = mbrT;
+		System.out.println(calc(exp,3,rtn));
 	}
 
+	public static double calc(Expression exp, int numDimensions, RTreeNode node){
+		double min = Double.POSITIVE_INFINITY;
+		double[] comb = new double[numDimensions];
+		min = calcRecursively(exp,numDimensions,0,min,node.MBR_S,node.MBR_T,comb);
+		return min;
+	}
+
+	private static double calcRecursively(Expression exp, int numDimensions, int curr, 
+			double min, double[] mbr_S,double[] mbr_T, double[] comb) {
+		if(curr == numDimensions-1){//at the last
+			double newV;
+			//MBR_S
+			comb[curr] = mbr_S[curr];
+			for(int i=1; i<=numDimensions; i++){
+				String varN = "x"+i;
+				exp.setVariable(varN, comb[i-1]);
+			}
+			newV = exp.evaluate();
+			//System.out.println(newV);
+			if(newV<min){
+				min=newV;
+			}
+			//MBR_T
+			comb[curr] = mbr_T[curr];
+			for(int i=1; i<=numDimensions; i++){
+				String varN = "x"+i;
+				exp.setVariable(varN, comb[i-1]);
+			}
+			newV = exp.evaluate();
+			//System.out.println(newV);
+			if(newV<min){
+				min=newV;
+			}
+			return min;
+		}else{//fill into comb[curr] and goto next
+			//MBR_S
+			comb[curr] = mbr_S[curr];
+			min = calcRecursively(exp,numDimensions,curr+1,min,mbr_S,mbr_T,comb);
+			//MBR_T
+			comb[curr] = mbr_T[curr];
+			return calcRecursively(exp,numDimensions,curr+1,min,mbr_S,mbr_T,comb);
+		}
+	}
 }
--- src/gensearch/MinMaxDist.java
+++ src/gensearch/MinMaxDist.java
@@ -1,10 +1,86 @@
 package gensearch;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
 public class MinMaxDist {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Expression exp = new ExpressionBuilder("x1*x2*x3").variables("x1","x2","x3").build();
+		double[] mbrS={1,2,3}, mbrT={4,5,6};
+		RTreeNode rtn = new RTreeNode();
+		rtn.MBR_S = mbrS;
+		rtn.MBR_T = mbrT;
+		System.out.println(calc(exp,3,rtn));
 	}
 
+	//1. get all vertexes into array
+	//2. sort all vertexes according to value in exp into a sorted array (desc order)
+	//3. for each hyperplane, get all vertexes on it(half of all in MBR)
+	//   then match them into the sorted array, and get the first one as max,
+	//   then compare to get the smallest one as MinMax
+	public static double calc(Expression exp, int numDimensions, RTreeNode node){
+		
+		//1.1 gen all indexes of all vertexes
+		ArrayList<Vertex> vertexes = new ArrayList<Vertex>();
+		genAllVertexes( numDimensions, 0, "", vertexes);
+		
+		//1.2 calc all vals of vertexes, and gen hashMap for them		 
+		vertexes.forEach(vertex->{
+			vertex.eval(exp,numDimensions,node.MBR_S,node.MBR_T);
+		});
+		
+		//2.1 sort (in increasing order)
+		Collections.sort(vertexes);
+		
+		//3.1 for each hyperplane, get all vertexes on it using excluding method!!!!
+		// and get the max of them using the sorted array
+		double min = Double.POSITIVE_INFINITY;
+		for(int i=0; i<numDimensions; i++){
+			double currMax=Double.POSITIVE_INFINITY;
+			//MBR_S
+			for(int j=vertexes.size()-1; j>=0; j--){
+				if(vertexes.get(j).index.charAt(i)=='0'){
+					currMax = vertexes.get(j).expVal;
+					break;
+				}
+			}
+			if(currMax<min){
+				min = currMax;
+			}
+			//MBR_T
+			for(int j=vertexes.size()-1; j>=0; j--){
+				if(vertexes.get(j).index.charAt(i)=='1'){
+					currMax = vertexes.get(j).expVal;
+					break;
+				}
+			}
+			if(currMax<min){
+				min = currMax;
+			}
+		}
+		
+		return min;
+	}
+
+	private static void genAllVertexes( int numDimensions, int currDim, String prefix, ArrayList<Vertex> vertexes) {
+		if(currDim == numDimensions-1){//last one, write into vertexes
+			String idx = prefix+"0";
+			Vertex v = new Vertex(idx);
+			vertexes.add(v);
+			idx = prefix+"1";
+			v = new Vertex(idx);
+			vertexes.add(v);
+		}else{//add prefix and goto next
+			genAllVertexes(numDimensions,currDim+1,prefix+"0",vertexes);
+			genAllVertexes(numDimensions,currDim+1,prefix+"1",vertexes);
+		}
+	}
+
+	
 }
+
+
